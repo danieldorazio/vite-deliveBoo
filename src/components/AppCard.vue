55782ee1d121a15dcd962ef08ad7abe28cca9bbe
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2024-02-19 12:11:42 +0100

    add: card test for category

---
+++ src/components/AppCard.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+    data(){
+        return{
+            category: [
+                {
+                    imagePath: "burger1.png",
+                    name: "Hamburger",
+                    description: "I migliori hamburger del mondo."
+                },
+                {
+                    imagePath: "burritos1.png",
+                    name: "Burrito",
+                    description: "I migliori burrito del mondo."
+                },
+                {
+                    imagePath: "pizza-slice1.png",
+                    name: "Pizza",
+                    description: "La miglior pizza del mondo."
+                },
+                {
+                    imagePath: "spaghetti1.png",
+                    name: "Spaghetti",
+                    description: "I migliori spaghetti del mondo."
+                },
+                {
+                    imagePath: "sawara1.png",
+                    name: "Sushi",
+                    description: "Che schifo il sushi"
+                },
+                {
+                    imagePath: "taco.png",
+                    name: "Cooking",
+                    description: "Non so cosa sia"
+                },
+                {
+                    imagePath: "dessert1.png",
+                    name: "Dessert",
+                    description: "Troppo buono"
+                },
+                {
+                    imagePath: "rice.png",
+                    name: "Poke",
+                    description: "blew"
+                },
+            ]
+        }
+    },
+    methods:{
+        getImagePath(path){
+            return new URL(`../assets/img/${path}`, import.meta.url).href;
+        },
+    }
+}
+</script>
+
+
+<template>
+    <div class="card mt-5 me-3" style="width: 18rem;" v-for="cat in category">
+        <div class="img-up text-center">
+            <img :src="getImagePath(cat.imagePath)" alt="">
+        </div>
+        <div class="card-body text-center">
+            <h5 class="card-title">{{ cat.name }}</h5>
+            <p class="card-text">{{ cat.description }}</p>
+        </div>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+.img-up{
+    height: 120px;
+    background: linear-gradient(90deg, #F8E16C 0%, #FAA343 100%);
+
+    img{
+        height: 100%;
+    }
+}
+</style>

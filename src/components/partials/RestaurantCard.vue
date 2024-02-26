5dd7a178ddae97058be24516f0dec5f6b52eac4e
cesarecicolin <cece.caster@gmail.com>
2024-02-26 15:51:26 +0100

    fix card

--- src/components/partials/RestaurantCard.vue
+++ src/components/partials/RestaurantCard.vue
@@ -33,23 +33,23 @@ export default {
             <p class="card-text"> {{ restaurant.time_open }} </p>
             <p class="card-text"> {{ restaurant.time_close }} </p>
         </div> -->
-        <router-link :to="{ name: 'show-restaurant' }" class="text-decoration-none text-black img-up rounded">
-            <img :src="getImagePath('rice.png')" alt="" class="card-img-top ">
-            <div class="card-body text-center position-relative">
+
+        
+        <!-- <router-link :to="{ name: 'show-restaurant' }" class="text-decoration-none text-black img-up rounded">
+         
+        </router-link> -->
+            <router-link :to="{name: 'show-restaurant', params: {id: restaurant.id }}" class="text-decoration-none text-black img-up rounded">
+                <img :src="getImagePath('rice.png')" alt="" class="card-img-top ">
+                <div class="card-body text-center position-relative">
+
                 <div class="position-absolute top-0 start-50 translate-middle btn btn-light rounded-pill">
                     <h5 class="card-title">{{ restaurant.restaurant_name }}</h5>
                 </div>
 
-
                 <p class="card-text mt-3 ">{{ restaurant.street }}</p>
             </div>
-        </router-link>
-
-        <div>
-            <!-- , params: {slug: restaurant.slug} -->
-            <p>page clicked restaurant</p>
-            <router-link :to="{name: 'show-restaurant', params: {id: restaurant.id }}" class="btn btn-success">Prova</router-link>
-        </div>
+            </router-link>
+       
        
     </div>
 </template>
